0e61fc287a30e9d82658ef8f6be8c4da99a2c700
nish <nishantpatel611@gmail.com>
2019-08-17 16:35:53 -0400

    add express api

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const data = require("./data/products.json");
 
 const app = express();
@@ -20,6 +21,25 @@ app.get("/api/products", (req, res) => {
       };
     })
   });
+});
+
+app.get("/api/express/whatshot", (req, res) => {
+  const { page = 1, per_page = 60 } = req.query;
+  axios
+    .get(
+      `https://endeca.search.unbxd.io/b3094e45838bdcf3acf786d57e4ddd98/express_com-u1456154309768/category/cat120002?page=${page}&per_page=${per_page}`
+    )
+    .then(expressRes => {
+      res.json(expressRes.data);
+    });
+});
+
+app.get("/api/express/product/:id", (req, res) => {
+  axios
+    .get(`https://www.express.com/api/v2/product/${req.params.id}/detail`)
+    .then(expressRes => {
+      res.json(expressRes.data);
+    });
 });
 
 app.listen(port, () => {
