2c79b127f17ac5ec269f9f694c358f0fff429135
Bhumi Patel <bhumip214@gmail.com>
2019-08-21 20:20:18 -0700

    updated Navbar with NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+
 export function Navbar(props) {
   return (
     <div className="navbar">
-      <h2 className="title"> Clothing Shop</h2>
-      <button className="shopping-cart-btn">
+      <NavLink to="/" className="title">
+        Clothing Shop
+      </NavLink>
+      <NavLink
+        to="/cart"
+        className="shopping-cart-link"
+        onClick={props.handleCartClick}
+      >
         <img
           className="shopping-cart"
           src="https://img.icons8.com/ios-filled/30/000000/shopping-cart.png"
         />
         <h3 className="cart-item-count">{props.count}</h3>
-      </button>
+      </NavLink>
     </div>
   );
 }
