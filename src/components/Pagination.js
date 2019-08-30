016a6b5424e6db24d27b1a6d9881aee6db07b8dd
Bhumi Patel <bhumip214@gmail.com>
2019-08-30 11:39:30 -0700

    added NavLink to pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,19 +1,21 @@
 import React from "react";
 import classnames from "classnames";
-
+import { NavLink } from "react-router-dom";
 export function Pagination(props) {
   let paginationBtns = [<button className="page-btn"> {"<"} </button>];
   for (let i = 1; i <= props.totalPages; i++) {
     paginationBtns.push(
-      <button
-        key={i}
-        className={classnames("page-btn", {
-          active: props.currPage === i
-        })}
-        onClick={() => props.handleGoToPage(i)}
-      >
-        {i}
-      </button>
+      <NavLink to={`?page=${i}&sort=${props.sort}`}>
+        <button
+          key={i}
+          className={classnames("page-btn", {
+            active: props.currPage === i
+          })}
+          onClick={() => props.handleGoToPage(i)}
+        >
+          {i}
+        </button>
+      </NavLink>
     );
   }
   paginationBtns.push(<button className="page-btn"> {">"} </button>);
