75a29b5e71d1212eca77ae2f54ef713cdcff15f5
Bhumi Patel <bhumip214@gmail.com>
2019-08-30 08:15:54 -0700

    add support for size and color filters

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const querystring = require("query-string");
 const data = require("./data/products.json");
 
 const app = express();
@@ -25,14 +26,29 @@ app.get("/api/products", (req, res) => {
 
 app.get("/api/express/whatshot", (req, res) => {
   // sort can be empty string or 'skuLowPrice' or 'skuHighPrice' or 'startDate'
-  const { page = 1, per_page = 60, sort = "" } = req.query;
-  axios
-    .get(
-      `https://endeca.search.unbxd.io/b3094e45838bdcf3acf786d57e4ddd98/express_com-u1456154309768/category/cat120002?page=${page}&per_page=${per_page}&sort=${sort}`
-    )
-    .then(expressRes => {
-      res.json(expressRes.data);
-    });
+  const {
+    page = 1,
+    per_page = 60,
+    sort = "",
+    size_uFilter = [],
+    color_uFilter = []
+  } = req.query;
+
+  const query = querystring.stringify({
+    page,
+    per_page,
+    sort,
+    size_uFilter: size_uFilter.join("|"),
+    color_uFilter: color_uFilter.join("|")
+  });
+
+  console.log({ query });
+
+  const searchUrl = `https://endeca.search.unbxd.io/b3094e45838bdcf3acf786d57e4ddd98/express_com-u1456154309768/category/cat120002?${query}`;
+
+  axios.get(searchUrl).then(expressRes => {
+    res.json(expressRes.data);
+  });
 });
 
 app.get("/api/express/product/:id", (req, res) => {
