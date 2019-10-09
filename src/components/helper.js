9957492fc04f517bc70844f984864285e206d0d1
Bhumi Patel <bhumip214@gmail.com>
2019-10-08 18:15:49 -0700

    updated API URLs to be dynamic based on window location

--- src/components/helper.js
+++ src/components/helper.js
@@ -1,15 +1,17 @@
 import queryString from "query-string";
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8001"
+    : "https://clothingshop-bp.herokuapp.com";
 
 export function fetchProducts(page, sort, color, size) {
   const c = queryString.stringify({ color: color });
   const s = queryString.stringify({ size: size });
   return fetch(
-    `http://localhost:8001/api/express/whatshot?page=${page}&sort=${sort}&${c}&${s}`
+    `${baseURL}/api/express/whatshot?page=${page}&sort=${sort}&${c}&${s}`
   ).then(res => res.json());
 }
 
 export function fetchProduct(id) {
-  return fetch(`http://localhost:8001/api/express/product/${id}`).then(res =>
-    res.json()
-  );
+  return fetch(`${baseURL}/api/express/product/${id}`).then(res => res.json());
 }
